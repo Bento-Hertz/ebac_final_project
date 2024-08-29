6b827e4e150c829693b178c2e1bca84d31d27dd9
Bento-Hertz <bentoluiz50@gmail.com>
2024-08-28 23:22:31 -0300

    :sparkles: feat: adding restaurant data fetching and changing props names

--- src/pages/Restaurant/index.tsx
+++ src/pages/Restaurant/index.tsx
@@ -1,30 +1,68 @@
+import { useEffect, useState } from 'react';
 import { IRestaurant } from '../../interfaces/IRestaurant';
 import Dish from './Dish';
 import * as S from './styles';
-import restaurants from 'data.json';
+import DishModal from './DishModal';
+import { useParams } from 'react-router-dom';
+import { IDish } from 'interfaces/IDish';
 
 function Restaurant() {
-    const { name, about, bgImage, dishes }: IRestaurant = restaurants[1];
+    const { id } = useParams();
+    
+    const [restaurant, setRestaurant] = useState<IRestaurant>();
 
+    useEffect(() => {
+
+        const getRestaurant = async () => {
+            try {
+                const restaurant: IRestaurant = await fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`).then((res) => res.json())
+                setRestaurant(restaurant);
+            }
+            catch(err) {
+                alert(err);
+            }
+        }
+        getRestaurant();
+    }, [id]);
+
+    const [currentModal, setCurrentModal] = useState<IDish>();
+    const [isModalActive, setIsModalActive] = useState<boolean>(false);
+
+    function invokeModal(dishId: number) {
+        const dishModal = restaurant?.cardapio.find((item) => item.id === dishId);
+        setCurrentModal(dishModal);
+        setIsModalActive(true);
+    }
+
+    if(!restaurant) {
+        return <></>
+    }
     return (
-        <S.Restaurant>
-            <S.Hero className='container overlay' imageSrc={bgImage}>
-                <ul>
-                    {about.map((item, index) => (
-                        <li key={index}>{item}</li>
+        <>
+            {currentModal && isModalActive ? 
+                <DishModal 
+                    dish={currentModal} 
+                    closeModal={() => setIsModalActive(false)}
+                />
+            :
+                <></>
+            }
+            <S.Restaurant>
+                <S.Hero className='container overlay' src={restaurant.capa}>
+                    <span>{restaurant.tipo}</span>
+                    <h1>{restaurant.titulo}</h1>
+                </S.Hero>
+                <S.Dishes>
+                    {restaurant.cardapio.map((dish) => (
+                        <Dish 
+                            key={dish.id}
+                            dish={dish}
+                            openModal={(dishId) => invokeModal(dishId)}
+                        />
                     ))}
-                </ul>
-                <h1>{name}</h1>
-            </S.Hero>
-            <S.Dishes>
-                {dishes.map((dish, index) => (
-                    <Dish 
-                        key={index}
-                        dish={dish}
-                    />
-                ))}
-            </S.Dishes>
-        </S.Restaurant>
+                </S.Dishes>
+            </S.Restaurant>
+        </>
     );
 }
 
