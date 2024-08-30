64c3f6c6d979bba9159ae44eee8939d292fe0795
Bento-Hertz <bentoluiz50@gmail.com>
2024-08-30 03:16:30 -0300

    :sparkles: feat: adding StatusMessage component to main jsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import Router from './pages/routes';
 import { GlobalStyle } from './styles';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Cart from 'components/Cart';
+import StatusMessage from 'components/StatusMessage';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <StatusMessage />
       <Header />
+      <Cart />
       <main className='container'>
         <Router />
       </main>
