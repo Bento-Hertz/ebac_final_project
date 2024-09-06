5e5dcd537b5a4c17563e196d39d1880119153942
Bento-Hertz <bentoluiz50@gmail.com>
2024-09-05 22:09:47 -0300

    :sparkles: feat: adding page loader and error message

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,24 +2,41 @@ import * as S from './styles';
 import RestaurantLink from './RestaurantLink';
 import { useEffect, useState } from 'react';
 import { IRestaurant } from 'interfaces/IRestaurant';
+import PageLoader from 'components/Loaders/PageLoader';
+import { useDispatch } from 'react-redux';
+import { updateStatusMessage } from 'store/reducers/statusMessage';
 
 function Home() {
+    const dispatch = useDispatch();
 
     const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
         const getRestaurants = async () => {
             try {
-                const restaurants = await fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes').then((res) => res.json())
+                setIsLoading(true);
+                const restaurants = await fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes').then((res) => res.json());
                 setRestaurants(restaurants);
             }
             catch(err) {
-                alert(err);
+                dispatch(updateStatusMessage({
+                    type:'error', 
+                    message:'Não foi possível carregar os restaurantes. Tente novamente mais tarde', 
+                    isActive:true
+                }));
+            }
+            finally {
+                setIsLoading(false);
             }
         }
         getRestaurants();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    if(isLoading) {
+        return <PageLoader />
+    }
     return (
         <S.Home>
             {restaurants.map((restaurant, index) => (
